f05aa5112c4f97c4579537470c069eca5dc33782
092vk <vk092kumar@gmail.com>
2024-03-03 01:37:49 +0530

    a

---
+++ day01/index.js
@@ -0,0 +1,32 @@
+console.log("hi i am the backend");
+
+
+// server is a software --- > which serves response for requests
+
+//frontend --> apis(structure and rules of communication) --> backend --> db (**another continent ***)
+
+
+// type of things backend deals with --> data , file,thirdPartyAPIS
+
+
+// file structure
+// 1.package.json
+// 2..env
+// 3.readme, git ,lint ,prettier 
+
+// -->src
+// index -- entryPoint (contains entry of db)
+// app -- logic , config , cookie etc
+// constants -- enums ,DB-name , (aeroplane seats)
+
+
+// directory structure 
+// -->DB : conatins actual code to connect the db, make requests and get response 
+// -->Models : contains schema 
+// -->Controllers: methods,functions
+// -->ROuters:different routes 
+// -->Middlewares: the part which is between two routes 
+// -->Utils: utilities
+// -->More(depends): 
+
+
